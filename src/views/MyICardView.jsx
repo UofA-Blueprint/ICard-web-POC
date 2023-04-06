388f349527d35d35601b3501048cfc86b3836257
Dimas Sudjito <almuhtad@ualberta.ca>
2023-04-05 18:43:23 -0600

    imported components without @env dependencies

---
+++ src/views/MyICardView.jsx
@@ -0,0 +1,141 @@
+import React, {useContext, useEffect, useState, } from 'react';
+import { View,  RefreshControl, StyleSheet,  ScrollView, ActivityIndicator } from 'react-native';
+import AuthContext from '../context/AuthContext';
+import MyICardPage from '../components/shared/ICardPage';
+import VerificationView from './VerificationView'
+import SubmittedView from './SubmittedView';
+import VendorView from './VendorView';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import VerifcationView from './VerificationView';
+
+
+let finalStatus;
+let message;
+
+const Stack = createNativeStackNavigator();
+
+import {API_ROUTE, API_KEY} from '@env';
+import { storeUser } from '../utilites/StoreUser';
+
+const authRoute = API_ROUTE;
+const apiKey = API_KEY;
+
+const MyICard = ({navigation}) => {
+  const {user, setUser} = useContext(AuthContext);
+  const [checkStatus, setCheckStatus] = useState(true);
+  const [refreshing, setRefreshing] = useState(false);
+
+
+  const loadUserData = async () => {
+
+    setRefreshing(true);
+    fetch(authRoute + `api/students/${user.id}`, {
+      method: 'GET',
+      headers: {'x-api-key': apiKey, 'jwt-token': user.key},
+    })
+    .then(result => {
+      return result.json();
+    })
+    .then(data => {
+      data["key"] = user.key;
+      data["id"] = data["_id"];
+      //Data being retrieved from backend is weird. If completely new user logs in
+      //then verification image is undefined and does not show up in the fetched user data obj
+      //If verifcation image was added to user at somepoint and then erased (so field is blank now), suddenly
+      //verification image field is not undefined and is just recognised as a blank space in
+      //the fetched user obj
+      //Line below is a work around
+      //if verification field is blank space or undefined, label it as empty/blank space
+      data["verification_image"] == ""  || data["verification_image"] == undefined ? data["verification_image"] = "" : null;
+      delete data["_id"];
+      setRefreshing(false);
+      setUser(data);
+      storeUser(data);
+      setCheckStatus(true);
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+}
+
+if (user == null) return <></>;
+
+
+const statusCheck = () => {
+  if (user == null) return <></>;
+  if (user.verify) finalStatus = 'inactive';
+  if (user.isaf_status && user.verify_status) finalStatus = 'active';
+  if (!user.isaf_status && user.verify_status) finalStatus = 'inactive, reverify';  //reverify
+
+  if (!user.isaf_status && !user.verify_status && !user.verification_image) finalStatus = 'inactive, verify'; //verify
+  if (!user.isaf_status && !user.verify_status && user.verification_image)
+
+    finalStatus = 'verifying account';
+    message = 'Verification in progress, may take up to 1-3 business days';
+  }
+
+
+  
+
+
+ 
+useEffect(() => {
+  if (checkStatus){
+    statusCheck();
+    setCheckStatus(false);
+  }
+}, [checkStatus])    
+
+
+  //verify button below is a todo. Just have a console.log in it for now
+  return (
+    <View style = {styles.container}>
+      <ScrollView data-testid = {'refreshControl'} refreshControl={<RefreshControl refreshing = {refreshing} onRefresh = {loadUserData}/>} 
+        contentContainerStyle={styles.contentContainer}>
+        <MyICardPage
+          user={user}
+          status={finalStatus}
+          msg = {message}
+          verify={() => {
+            navigation.navigate('Verification');
+          }}
+        />
+      </ScrollView>
+    </View>
+  );
+};
+      
+const MyICardView = () => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        name="My ICard"
+        component={MyICard}
+        options={{headerShown: false}}
+      />
+      <Stack.Screen
+        name="Verification"
+        component={VerifcationView}
+        options={{headerShown: false}}
+      />
+      <Stack.Screen
+        name="Submitted"
+        component={SubmittedView}
+        options={{headerShown: false}}
+      />
+    </Stack.Navigator>
+  );
+};
+
+const styles = StyleSheet.create({
+ 
+  container:{
+    flex:1
+  },
+  contentContainer: {
+    flex: 1
+  }
+})
+
+export default MyICardView;
---
+++ src/views/VendorView.jsx
@@ -0,0 +1,73 @@
+import React from 'react';
+import axios from 'axios';
+import {StyleSheet, View, FlatList, Text, Dimensions, ImageBackground} from 'react-native';
+import Header from '../components/shared/Header';
+import {globalStyleSheet} from '../utilites/Theme';
+import {colors} from '../utilites/Theme'
+import {useState, useEffect} from 'react';
+import SearchBar from '../components/shared/SearchBar';
+import VendorList from '../components/shared/VendorList';
+import { shuffle } from '../utilites/Shuffle';
+import {options, url} from '../data/vendorMockData'
+
+  const VendorView = () => {
+    const [searchPhrase, setSearchPhrase] = useState('');
+    const [clicked, setClicked] = useState(false);
+    const [vendorData, setList] = useState([]);
+
+  React.useEffect(() => {
+    axios.get(url, options)
+          .then((res) => {
+          setList(res.data)
+        })
+        .catch((error) => console.log(error))
+  }, []);
+
+
+    return (
+      <ImageBackground source={require('../../assets/Background.png')} style={styles.backgoundImage}>
+        <Text style={{
+          color: colors.primary,
+          fontSize: 35,
+          fontWeight: 'bold',
+          marginTop: 64,
+          marginHorizontal: 24,
+          alignSelf: 'flex-start'
+        }}>
+          Vendors
+        </Text>
+
+        <SearchBar
+          searchPhrase={searchPhrase}
+          setSearchPhrase={setSearchPhrase}
+          clicked={clicked}
+          setClicked={setClicked}
+        />
+
+        <VendorList
+          searchPhrase={searchPhrase}
+          data={vendorData}
+          setClicked={setClicked}
+        />
+
+      </ImageBackground>
+    );
+  };
+
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      paddingTop: 64,
+      backgroundColor: "#D9FFDC44",
+      alignItems: 'center',
+    },
+    backgoundImage:{
+      flex: 1,
+      resizeMode: 'cover'
+    },
+    vendorList: {
+      paddingHorizontal: 20,
+    },
+  });
+
+export default VendorView;
